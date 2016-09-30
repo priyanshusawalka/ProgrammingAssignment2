dbdbbeca843dd9359161b5ec726eb0b332cf278a
priyanshusawalka <priyanshu_sawalka@mastercard.com>
2016-09-30 14:21:08 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
-## First draft
-## Put comments here that give an overall description of what your
-## functions do
+## second draft
+
+## Caching the Inverse of a Matrix:
+## Matrix inverssion is usually a costly computation and there may be some 
+## benefit to caching the inverseerse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
 
 ## Write a short comment describing this function
 
